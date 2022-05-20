9e92445adea336c1789b8d0dc8942bd704067f17
Roman Kalashnikov <lunix0x@gmail.com>
2022-05-20 16:08:17 +0300

    Resolve valid parentheses solution

--- src/valid_parentheses.cpp
+++ src/valid_parentheses.cpp
@@ -2,17 +2,17 @@
  * Description: https://leetcode.com/problems/valid-parentheses/
  * */
 #include "solutions.hpp"
-#include <iostream>
 #include <map>
 #include <stack>
+
 
 bool SLN::isValidParentheses(std::string s) {
   std::stack<char> buffer;
   std::map<char, char> closeToOpen = {{')', '('}, {'}', '{'}, {']', '['}};
 
-  for (auto p : s) {
-    if (closeToOpen.find(p) == closeToOpen.end()) {
-      if (!buffer.empty() && buffer.top() == closeToOpen[p]) {
+  for (const auto p : s) {
+    if (closeToOpen.find(p) != closeToOpen.end()) {
+      if (!buffer.empty() && (buffer.top() == closeToOpen[p])) {
         buffer.pop();
       } else {
         return false;
--- tests/test_valid_parentheses.cpp
+++ tests/test_valid_parentheses.cpp
@@ -2,12 +2,12 @@
 #include <gtest/gtest.h>
 
 TEST(valid_parentheses, positive) {
-    EXPECT_TRUE(SLN::isPalindrome("()"));
-    EXPECT_TRUE(SLN::isPalindrome("()[]{}"));
-    EXPECT_TRUE(SLN::isPalindrome("((()))"));
+    EXPECT_TRUE(SLN::isValidParentheses("()"));
+    EXPECT_TRUE(SLN::isValidParentheses("()[]{}"));
+    EXPECT_TRUE(SLN::isValidParentheses("((()))"));
 }
 
 TEST(valid_parentheses, negative) {
-    EXPECT_FALSE(SLN::isPalindrome("(]"));
-    EXPECT_FALSE(SLN::isPalindrome("((())"));
+    EXPECT_FALSE(SLN::isValidParentheses("(]"));
+    EXPECT_FALSE(SLN::isValidParentheses("((())"));
 }
